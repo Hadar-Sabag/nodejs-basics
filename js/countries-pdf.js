0f582012bdc0aab140fc0cd88fc670eb3455ea45
hadar <hadarsabag5@gmail.com>
2025-04-21 18:30:40 +0300

    Add countries-pdf

--- js/countries-pdf.js
+++ js/countries-pdf.js
@@ -1,24 +1,32 @@
 import fs from 'fs'
 import PDFDocument from 'pdfkit-table'
+import { utilService } from './util.service.js'
 
-// init document
+const countries = utilService.readJsonFile('../countries.json')
+
+console.log('countries', countries)
+
+
+countries.sort((a, b) => a.name.common.localeCompare(b.name.common))
+
+
+
 let doc = new PDFDocument({ margin: 30, size: 'A4' })
-
-// connect to a write stream
 doc.pipe(fs.createWriteStream('./students.pdf'))
-createPdf(doc)
- .then(() => doc.end()) // close document
- 
-function createPdf() {
-const table = {
-title: 'Students',
-subtitle: 'Sorted by age',
-headers: ['Name', 'Age', 'Grade'],
-rows: [
- ['Donni', '22', '10'],
- ['Ronni', '25', '9'],
- ['Boris', '27', '10'],
- ],
-}
-return doc.table(table, { columnsSize: [200, 100, 100] })
+
+createPdfTable(doc, countries)
+    .then(() => doc.end())
+
+function createPdfTable(doc, countries) {
+    const table = {
+        title: 'Countries of the World',
+        subtitle: 'Sorted by name',
+        headers: ['Name', 'Capital', 'Population'],
+        rows: countries.map(country => [
+            country.name.common,
+            country.capital?.[0] || 'N/A',
+            country.population.toLocaleString()
+        ])
+    }
+    return doc.table(table, { columnsSize: [200, 150, 150] })
 }
